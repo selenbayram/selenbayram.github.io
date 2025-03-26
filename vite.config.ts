b1b2b90f5b74c7afb5c1113cd515bdfa57b87545
Selen Bayram <selen.bayram@mail.utoronto.ca>
2025-03-26 00:26:51 -0400

    Added GitHub Pages deployment configurations

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/selenbayram.github.io/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
